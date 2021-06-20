ba7ba66716ec8b09babb1b9b4d7c2e698c8161df
Amir Ali Tashk <amirali.tashk@gmail.com>
2021-06-20 16:42:07 +1000

    test by Ali

--- hangman.py
+++ hangman.py
@@ -6,13 +6,13 @@ num2 = 5
 sum = num1 + num2
 
 print('answer is' .format(num1, num2) )
-<<<<<<< HEAD
+## HEAD
 print('making new changes')
 
-=======
+##=======
 
 #this is a testing line
->>>>>>> 72d1df2c693473b672b5f332c41147020577d5f8
+## 72d1df2c693473b672b5f332c41147020577d5f8
 
 ##a;soeifj;asiefj
 ##;iofjse
